2847232280ed410a78a2d7fe3266abf29d08d83a
zak-dev-art <zachndungu861@gmail.com>
2025-09-25 13:40:43 +0300

    modified js file

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((object) => {
+      // Append the new id to the DOM
+      document.body.innerHTML = object.id;
+    })
+    .catch((error) => {
+      // Append the error message to the DOM
+      document.body.innerHTML = error.message;
+    });
+}
